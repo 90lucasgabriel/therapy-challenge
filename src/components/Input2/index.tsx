2deb637bccb975b575617762d4eecbbfb7c13b1f
Lucas Gabriel <90lucasgabriel@gmail.com>
2023-01-14 22:53:49 -0300

    03 - New: Button and Input components; 03 - New: Auth context; 03 - New: Signin screen;

---
+++ src/components/Input2/index.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { TextInput } from 'react-native';
+
+import InputProps from 'components/Input2/types';
+import { Container, Label } from './styles';
+
+const Input = ({ label }: InputProps) => {
+  return (
+    <Container>
+      <Label>{label}</Label>
+      <TextInput />
+    </Container>
+  );
+};
+
+export default Input;
