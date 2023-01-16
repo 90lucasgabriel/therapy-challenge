b40c7b6717d53a2f153cce580099431f0896d49c
Lucas Gabriel <90lucasgabriel@gmail.com>
2023-01-15 21:03:49 -0300

    03 - New: Env and Firebase;

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -2,19 +2,26 @@ import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 
-import ENV from 'react-native-config';
+import {
+  REACT_APP_FIREBASE_API_KEY,
+  REACT_APP_FIREBASE_AUTH_DOMAIN,
+  REACT_APP_FIREBASE_PROJECT_ID,
+  REACT_APP_FIREBASE_STORAGE_BUCKET,
+  REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  REACT_APP_FIREBASE_APP_ID,
+} from '@env';
 
 const firebaseConfig = {
-  apiKey: `${ENV?.REACT_APP_FIREBASE_API_KEY}`,
-  authDomain: `${ENV?.REACT_APP_FIREBASE_AUTH_DOMAIN}`,
-  projectId: `${ENV?.REACT_APP_FIREBASE_PROJECT_ID}`,
-  storageBucket: `${ENV?.REACT_APP_FIREBASE_STORAGE_BUCKET}`,
-  messagingSenderId: `${ENV?.REACT_APP_FIREBASE_MESSAGING_SENDER_ID}`,
-  appId: `${ENV?.REACT_APP_FIREBASE_APP_ID}`,
+  apiKey: REACT_APP_FIREBASE_API_KEY,
+  authDomain: REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: REACT_APP_FIREBASE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
 const authFirebase = getAuth(app);
-const db = getFirestore(app);
+const db = getFirestore();
 
 export { app, authFirebase, db };
